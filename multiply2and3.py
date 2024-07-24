3772d7e2bf68f8b5314618dfdfa4674a1d38c8a8
Sargun Singh <sargun.singh@beehyv.com>
2024-07-24 12:26:33 +0530

    900

--- multiply2and3.py
+++ multiply2and3.py
@@ -100,13 +100,13 @@ for _ in range(t):
     cnt2, cnt3 = 0, 0
     if n == 1:
         print(0)
-    while n > 0:
-        while n % 2:
-            n = n // 2
-            cnt2 += 1
-        while n % 3:
-            n = n // 3
-            cnt3 += 1
+
+    while n % 2 == 0:
+        n = n // 2
+        cnt2 += 1
+    while n % 3 == 0:
+        n = n // 3
+        cnt3 += 1
 
     if n == 1 and cnt2 <= cnt3:
         print(2*(cnt3 - cnt2))
